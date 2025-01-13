7289740bb1f7916e943d41700c138528cf81dce6
Visheshyadav8652 <162328787+Visheshyadav8652@users.noreply.github.com>
2025-01-13 20:20:37 +0530

    add OAuth funcationality

--- api/models/user.models.js
+++ api/models/user.models.js
@@ -16,6 +16,10 @@ const uesrSchemea = new mongoose.Schema(
             type: String,
             required: true,
         },
+        profilePicture:{
+            type:String,
+            default:"https://th.bing.com/th/id/OIP.c5KXw-wPcnwyyBNayoXfFQHaHa?w=179&h=180&c=7&r=0&o=5&dpr=1.4&pid=1.7",
+        },
     }, { timestamps: true }
 );
 
