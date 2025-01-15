08f4a55467600ba24e177c8089f65b17a4436944
Visheshyadav8652 <162328787+Visheshyadav8652@users.noreply.github.com>
2025-01-15 10:57:27 +0530

    add signout api routing

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -58,10 +58,20 @@ export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You are not allowed to delete this user'));
   }
-  try{
+  try {
     await User.findByIdAndDelete(req.params.userId);
     res.status(200).json('User has been deleted');
-  }catch(error){
+  } catch (error) {
     next(error);
   }
 };
+
+export const signout = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token')
+      .status(200)
+      .json('User has been signed out');
+  } catch (error) {
+    next(error);
+  }
+}
